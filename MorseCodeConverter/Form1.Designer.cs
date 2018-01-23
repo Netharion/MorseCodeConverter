9607b3b655ed608d4db4dd19473edb5c7118ce2f
Netharion <anthonym91@gmail.com>
2018-01-23 18:53:09 -0500

    Add project files.

---
+++ MorseCodeConverter/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace MorseCodeConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputGroupBox = new System.Windows.Forms.GroupBox();
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.outputGroupbox = new System.Windows.Forms.GroupBox();
+            this.morseTextBox = new System.Windows.Forms.TextBox();
+            this.descLabel = new System.Windows.Forms.Label();
+            this.convertButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.inputGroupBox.SuspendLayout();
+            this.outputGroupbox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // inputGroupBox
+            // 
+            this.inputGroupBox.Controls.Add(this.inputTextBox);
+            this.inputGroupBox.Location = new System.Drawing.Point(12, 52);
+            this.inputGroupBox.Name = "inputGroupBox";
+            this.inputGroupBox.Size = new System.Drawing.Size(652, 155);
+            this.inputGroupBox.TabIndex = 0;
+            this.inputGroupBox.TabStop = false;
+            this.inputGroupBox.Text = "Input";
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputTextBox.Location = new System.Drawing.Point(7, 20);
+            this.inputTextBox.Multiline = true;
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(639, 129);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // outputGroupbox
+            // 
+            this.outputGroupbox.Controls.Add(this.morseTextBox);
+            this.outputGroupbox.Location = new System.Drawing.Point(12, 213);
+            this.outputGroupbox.Name = "outputGroupbox";
+            this.outputGroupbox.Size = new System.Drawing.Size(652, 237);
+            this.outputGroupbox.TabIndex = 1;
+            this.outputGroupbox.TabStop = false;
+            this.outputGroupbox.Text = "Output";
+            // 
+            // morseTextBox
+            // 
+            this.morseTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.morseTextBox.Location = new System.Drawing.Point(7, 20);
+            this.morseTextBox.Multiline = true;
+            this.morseTextBox.Name = "morseTextBox";
+            this.morseTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.morseTextBox.Size = new System.Drawing.Size(645, 211);
+            this.morseTextBox.TabIndex = 0;
+            // 
+            // descLabel
+            // 
+            this.descLabel.AutoSize = true;
+            this.descLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.descLabel.Location = new System.Drawing.Point(87, 20);
+            this.descLabel.Name = "descLabel";
+            this.descLabel.Size = new System.Drawing.Size(480, 20);
+            this.descLabel.TabIndex = 2;
+            this.descLabel.Text = "Enter text in the Input field to have the Morse Code Output";
+            // 
+            // convertButton
+            // 
+            this.convertButton.Location = new System.Drawing.Point(166, 467);
+            this.convertButton.Name = "convertButton";
+            this.convertButton.Size = new System.Drawing.Size(108, 40);
+            this.convertButton.TabIndex = 3;
+            this.convertButton.Text = "Convert";
+            this.convertButton.UseVisualStyleBackColor = true;
+            this.convertButton.Click += new System.EventHandler(this.convertButton_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(394, 467);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(108, 40);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(280, 467);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(108, 40);
+            this.clearButton.TabIndex = 5;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 517);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.convertButton);
+            this.Controls.Add(this.descLabel);
+            this.Controls.Add(this.outputGroupbox);
+            this.Controls.Add(this.inputGroupBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.inputGroupBox.ResumeLayout(false);
+            this.inputGroupBox.PerformLayout();
+            this.outputGroupbox.ResumeLayout(false);
+            this.outputGroupbox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox inputGroupBox;
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.GroupBox outputGroupbox;
+        private System.Windows.Forms.TextBox morseTextBox;
+        private System.Windows.Forms.Label descLabel;
+        private System.Windows.Forms.Button convertButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ MorseCodeConverter/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorseCodeConverter
+{
+    
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //string method that accepts argument character M
+        private string morseConverter(char M)
+        {
+            //initialize variable char input equal to Parsed char of M.ToString()
+            char input   = char.Parse(M.ToString());
+            switch (input)
+            {
+                //switch cases for each character available
+                //A-Z is a 4 digit code, Add space at end of code to seperate character
+                //?, Comma, and period are 6 morse digit add space to end
+                //in the case of a space return two spaces to seperate words
+                case ' ':
+                    return "  ";
+                case 'A':
+                    return ".-   ";
+                case 'B':
+                    return "-... ";
+                case 'C':
+                    return "-.-. ";
+                case 'D':
+                    return "-..  ";
+                case 'E':
+                    return ".    ";
+                case 'F':
+                    return "..-. ";
+                case 'G':
+                    return "--.  ";
+                case 'H':
+                    return ".... ";
+                case 'I':
+                    return "..   ";
+                case 'J':
+                    return ".--- ";
+                case 'K':
+                    return "-.-  ";
+                case 'L':
+                    return ".-.. ";
+                case 'M':
+                    return "--  "; //the book appears to state this is "--- ", even though other resources online state its just "--  "
+                case 'N':
+                    return "-.   ";
+                case 'O':
+                    return "---  ";
+                case 'P':
+                    return ".--. ";
+                case 'Q':
+                    return "--.- ";
+                case 'R':
+                    return ".-.  ";
+                case 'S':
+                    return "...  ";
+                case 'T':
+                    return "-    ";
+                case 'U':
+                    return "..-  ";
+                case 'V':
+                    return "...- ";
+                case 'W':
+                    return ".--  ";
+                case 'X':
+                    return "-..- ";
+                case 'Y':
+                    return "-.-- ";
+                case 'Z':
+                    return "--.. ";             
+                case '0':
+                    return "----- ";
+                case '1':
+                    return ".---- ";
+                case '2':
+                    return "..--- ";
+                case '3':
+                    return "...-- ";
+                case '4':
+                    return "....- ";
+                case '5':
+                    return "..... ";
+                case '6':
+                    return "-.... ";
+                case '7':
+                    return "--... ";
+                case '8':
+                    return "---.. ";
+                case '9':
+                    return "----. ";
+                case ',':
+                    return "--..-- ";
+                case '.':
+                    return ".-.-.- ";
+                case '?':
+                    return "..--.. ";
+                case '!': //Morse code table in book doesnt list !, used online resource to find this.
+                    return "-.-.-- ";
+                default:
+                    return "";
+            }
+        }
+
+
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+
+        private void convertButton_Click(object sender, EventArgs e)
+        {
+            //string inputText is equal to textbox text value passed ToUpper method to make all characters Capital
+            string inputText = (inputTextBox.Text).ToUpper(); 
+            morseTextBox.Clear(); //When convert button clicked morseTextbox is cleared to accept conversion
+
+            //foreach loop for each character in inputText
+            foreach (char letter in inputText)
+            {
+                //add morseConverter method with argument for letter in inputText                
+                morseTextBox.Text += morseConverter(letter);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            //clear both text boxes
+            morseTextBox.Clear();
+            inputTextBox.Clear();
+        }
+    }
+}
